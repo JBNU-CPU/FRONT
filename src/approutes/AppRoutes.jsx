3d8500ce9e3285691919ab610330eeab39ff4576
Coti00 <idjda1130@naver.com>
2025-02-13 15:59:20 +0900

    route

--- src/approutes/AppRoutes.jsx
+++ src/approutes/AppRoutes.jsx
@@ -4,7 +4,6 @@ import Main from '../pages/Main';
 import About from '../pages/About';
 import Community from '../pages/Community';
 import Gallery from '../pages/Gallery';
-import Join from '../pages/Join';
 import Login from '../pages/Login';
 import Notification from '../pages/Notification';
 import Recruit from '../pages/Recruit';
@@ -12,11 +11,9 @@ import Management from '../pages/Management';
 import Mypage from '../pages/Mypage';
 import Join2 from '../pages/Join2';
 import FindPassword from '../pages/FindPassword';
-import ReviseMemInfo from '../pages/ReviseMemInfo';
 import ReviseMemInfo2 from '../pages/ReviseMemInfo2';
 import StudyMainPage from '../pages/StudyMainPage';
 import GalleryDetail from '../pages/GalleryDetail';
-import Project from '../pages/Project';
 import SectionOpen from '../pages/SectionOpen';
 import StudyOpen from '../pages/StudyOpen';
 import ProjectOpen from '../pages/ProjectOpen';
@@ -39,37 +36,50 @@ const AppRoutes = () => {
         <Routes>
             <Route path='/' element={<Main />} />
             <Route path='/about' element={<About />} />
-            <Route path='/community' element={<Community />} />
+
             <Route path='/gallery' element={<Gallery />} />
             <Route path='/galleryDetail/:id' element={<GalleryDetail/>}/>
-            <Route path='/join' element={<Join />} />
+
+            <Route path='/recruit' element={<Recruit />} />
+            <Route path ='/mypage' element={<Mypage/>}/>
+
             <Route path='/join2' element={<Join2/>}/>
             <Route path='/login' element={<Login />} />
-            <Route path='/notification' element={<Notification />} />
-            <Route path='/recruit' element={<Recruit />} />
-            <Route path='/management' element={<Management />} />
-            <Route path ='/mypage' element={<Mypage/>}/>
             <Route path='/findpassword' element={<FindPassword/>}/>
-            <Route path='/revisememberinfo' element={<ReviseMemInfo/>}/>
             <Route path='/revisememberinfo2' element={<ReviseMemInfo2/>}/>
-            <Route path='/studymain' element={<StudyMainPage/>}/>
-            <Route path='/project' element={<Project/>}/>
-            <Route path='/sectionopen' element={<SectionOpen/>}/>
-            <Route path='/studyopen' element={<StudyOpen/>}/>
-            <Route path='/projectopen' element={<ProjectOpen/>}/>
-            <Route path='/noticontent' element={<NotiContent/>}/>
-            <Route path='/notiwrite' element={<NotiWrite/>}/>
-            <Route path='/write' element={<CommunityWrite/>}/>
-            <Route path='/content' element={<CommunityContent/>}/>
-            <Route path='/test' element={<Test/>}/>
-            <Route path='/studyinfo/:id' element={<Studyinfo/>}/>
-            <Route path='/sectioninfo/:id' element={<Sectioninfo/>}/>
-            <Route path='/projectinfo/:id' element={<Projectinfo/>}/>
-            <Route path='/event' element={<Event/>}/>
+
+            {/* 관리자페이지 경로 */}
+            <Route path='/management' element={<Management />} />
             <Route path='/usermanage' element={<UserManage/>}/>
             <Route path='/sectionmanage' element={<SectionManage/>}/>
             <Route path='/studymanage' element={<StudyManage/>}/>
             <Route path='/projectmanage' element={<ProjectManage/>}/>
+
+            {/* 커뮤니티 관련 경로 */}
+            <Route path='/community' element={<Community />} />
+            <Route path='/write' element={<CommunityWrite/>}/>
+            <Route path='/content' element={<CommunityContent/>}/>
+            
+            {/* 공지사항 관련 경로 */}
+            <Route path='/notification' element={<Notification />} />
+            <Route path='/noticontent' element={<NotiContent/>}/>
+            <Route path='/notiwrite' element={<NotiWrite/>}/>
+
+            {/* 스터디 관련 경로 */}
+            <Route path='/studymain' element={<StudyMainPage/>}/>
+
+            <Route path='/projectinfo/:id' element={<Projectinfo/>}/>
+            <Route path='/projectopen' element={<ProjectOpen/>}/>
+
+            <Route path='/sectionopen' element={<SectionOpen/>}/>
+            <Route path='/sectioninfo/:id' element={<Sectioninfo/>}/>
+
+            <Route path='/studyopen' element={<StudyOpen/>}/>
+            <Route path='/studyinfo/:id' element={<Studyinfo/>}/>
+
+            {/* 테스트페이지 경로*/}
+            <Route path='/test' element={<Test/>}/>
+            <Route path='/event' element={<Event/>}/>
 
         </Routes>
     );
